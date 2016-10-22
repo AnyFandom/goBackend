3745f195cae7f845a3bd8828f15d666162898a5a
lunavod <lunavod@bk.ru>
2016-10-22 22:14:32 +0300

    Added GORM

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,18 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"goBackend/app/models"
+
+	"github.com/revel/revel"
+)
 
 type App struct {
-	*revel.Controller
+	GormController
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	user := models.User{Name: "Jinzhup"}
+	c.Txn.NewRecord(user)
+	c.Txn.Create(&user)
+	return c.RenderJson(user)
 }
